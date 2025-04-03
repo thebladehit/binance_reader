a12db16af5f37f3373886781c235cf9c6824d7e8
thebladehit <boqdanyarmolka@gmail.com>
2025-04-03 15:47:26 +0300

    Remove pipelining from savePair fn

--- ws-reader/redis/redis.js
+++ ws-reader/redis/redis.js
@@ -1,21 +1,11 @@
 const Redis = require('ioredis');
-const { REDIS_PIPELINE_TIMEOUT } = require('../config/config');
 const redis = new Redis();
 
 const streamPrefix = 'binance';
 
-let pipeline = redis.pipeline();
-
 const savePair = async (pairName, data) => {
   const streamKey = `${streamPrefix}:${pairName}`;
-  pipeline.xadd(streamKey, '*', 'data', data);
+  redis.xadd(streamKey, '*', 'data', data);
 }
-
-setInterval(() => {
-  if (pipeline.length !== 0) {
-    pipeline.exec();
-    pipeline = redis.pipeline();
-  }
-}, REDIS_PIPELINE_TIMEOUT);
 
 module.exports = { savePair };
