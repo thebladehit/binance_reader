128cc2f45b52637b2cfb0a7a453b2c7986d6b6c6
thebladehit <boqdanyarmolka@gmail.com>
2025-04-05 16:27:12 +0300

    Change create-db script

--- api-server/sql-scripts/create-db.js
+++ api-server/sql-scripts/create-db.js
@@ -1,19 +1,40 @@
 const { Client } = require('pg');
-const fs = require('fs');
+const { getPairNames } = require('../../common/parser/parser');
+const { PAIRS_PATH } = require('../../common/constants/constanse');
+
+const tableSQL = `
+  CREATE TABLE IF NOT EXISTS trades_{{name}} (
+    tradeId BIGINT PRIMARY KEY,
+    price VARCHAR(255),
+    quantity VARCHAR(255),
+    timestamp BIGINT
+  );
+
+  CREATE INDEX IF NOT EXISTS idx_timestamp ON trades_{{name}} (timestamp);
+`;
 
 const client = new Client({
   host: '127.0.0.1',
   user: 'postgres', // your name
   password: '1111', // your pass
-  database: 'binance_data'
+  database: 'binance_data' // your db name
 });
 
 client.connect();
 
-const sql = fs.readFileSync('./db.sql', 'utf8');
+const createTables = async () => {
+  let sqlQuery = '';
 
-client.query(sql, (err, res) => {
-  if (err) throw err;
-  console.log('Table created successfuly');
-  client.end();
-});
+  const pairs = await getPairNames(PAIRS_PATH);
+  for (const pair of pairs) {
+    sqlQuery += tableSQL.replaceAll('{{name}}', pair) + '\n';
+  }
+
+  client.query(sqlQuery, (err, res) => {
+    if (err) throw err;
+    console.log('Table created successfuly');
+    client.end();
+  });
+};
+
+createTables();
